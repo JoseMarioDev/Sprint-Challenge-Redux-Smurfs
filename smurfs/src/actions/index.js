982b60b49a58fa5d4380e459293fb0b5b9789789
Jose Montero Jr <josemariodev@gmail.com>
2019-06-21 11:09:44 -0500

    reducer setup

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,21 +4,21 @@ export const FETCH_SMURF_START = 'FETCH_SMURF_START';
 export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
 export const FETCH_SMURF_ERROR = 'FETCH_SMURF_ERROR';
 
-Let nextSmurfId = 0;
+let nextSmurfId = 0;
 
 export const addSmurf = text => ({
   type: 'ADD_SMURF',
   id: nextSmurfId++,
   text,
-})
+});
 
 export const getSmurfs = name => dispatch => {
-  dispatch({type: FETCH_SMURF_START});
+  dispatch({ type: FETCH_SMURF_START });
   axios
-  .get('localhost:3333/smurfs')
-  .then(res=> {
-    console.log(res);
-    dispatch({type:FETCH_SMURF_SUCCESS, payload.res.data})
-  })
-  .catch(err=>dispatch({type:FETCH_SMURF_ERROR}))
+    .get('localhost:3333/smurfs')
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+    })
+    .catch(err => dispatch({ type: FETCH_SMURF_ERROR }));
 };
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,27 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  FETCH_SMURF_START,
+  FETCH_SMURF_SUCCESS,
+  FETCH_SMURF_ERROR,
+} from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smrufs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  error: null,
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const smurfsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_SMURF_START:
+      return { ...state, fetchingSmurfs: true, error: null };
+    case FETCH_SMURF_SUCCESS:
+      return { ...state, fetchingSmurfs: false, smurfs: action.payload };
+    case FETCH_SMURF_ERROR:
+      return { ...state, fetchingSmurfs: false, error: 'error, try again' };
+    default:
+      return state;
+  }
+};
+
+export default smurfsReducer;
